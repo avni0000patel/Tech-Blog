9be64456b4c48423482f5bdd1f329a95e5643b11
Avni <avni0000patel@gmail.com>
2022-09-03 20:25:07 -0400

    public/js/post.js

---
+++ public/js/post.js
@@ -0,0 +1,26 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector(`input[name="title"]`).value.trim();
+    const content = document.querySelector(`input[name="content"]`).value.trim();
+
+    if (title && content) {
+        const response = await fetch(`/api/posts`, {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.new-project-form')
+    .addEventListener('submit', newFormHandler);
