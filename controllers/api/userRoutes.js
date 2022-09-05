211d68a6d39d5fb69add01454fcfe1e7f8e0c97a
Avni <avni0000patel@gmail.com>
2022-09-05 13:11:54 -0400

    controllers/api/userRoutes.js

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,145 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+// Get all users
+router.get('/', (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }, { model: Comment }]
+        });
+
+        res.status(200).json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+// Get one user
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            attributes: { exclude: ['password'] },
+            where: {
+                id: req.params.id
+            },
+            include: [{ model: Post }, { model: Comment }]
+        });
+
+        if (!userData) {
+            res.status(404).json({ message: "No user found with this id!" });
+            return;
+        }
+
+        res.status(200).json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Create new user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!userData) {
+            res.status(400).json({ message: 'No user with that username!' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+// Update
+router.put('/:id', async (req, res) => {
+    try {
+        const userData = await User.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!userData) {
+            res.status(400).json({ message: 'No user found with this id!' });
+            return;
+        }
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await Product.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!userData) {
+            res.status(404).json({ message: "No user found with this id" });
+            return;
+        }
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+
