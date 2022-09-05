6bc8f46896e61d724842bfd88c0e510ca39398a9
Avni <avni0000patel@gmail.com>
2022-09-05 13:40:16 -0400

    controllers/api/commentRoutes.js

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,100 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+// Get all posts
+router.get('/', (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            include: [{ model: User }, { model: Post }]
+        });
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+// Get one post
+router.get('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.findByPk(req.params.id, {
+            where: {
+                id: req.params.id
+            },
+            include: [{ model: User }, { model: Post }]
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: "No comment found with this id!" });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Create new comment
+router.post('/', async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            comment: req.body.comment,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(commentData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Update
+router.put('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(400).json({ message: 'No comment found with this id!' });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete
+router.delete('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: "No comment found with this id" });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -98,4 +98,3 @@ router.delete('/:id', async (req, res) => {
 });
 
 module.exports = router;
-
