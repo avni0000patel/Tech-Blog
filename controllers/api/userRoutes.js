c855c4e6d7e2a31573aabd9655914cbb9df27642
Avni <avni0000patel@gmail.com>
2022-09-09 21:54:40 -0400

    working login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -78,8 +78,7 @@ router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({
             where: {
-                username,
-                password
+                username
             },
         });
 
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -23,6 +23,10 @@ const userData = [
     },
 ];
 
-const seedUsers = () => User.bulkCreate(userData);
+// const seedUsers = () => User.bulkCreate(userData);
+const seedUsers = () => User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+});
 
 module.exports = seedUsers;
